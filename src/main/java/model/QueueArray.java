fb58612c553c50886caa769fd2168602b301cfc0
sigma69-coder <sigmaboy77878@gmail.com>
2025-05-01 21:15:49 +0530

    Add the QueueArray class

--- src/main/java/model/QueueArray.java
+++ src/main/java/model/QueueArray.java
@@ -1,21 +1,51 @@
 package model;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 public class QueueArray {
-    private static Queue<Reservation> queue = new LinkedList<>();
+    private int maxSize;
+    private Reservation[] queArray;
+    private int front;
+    private int rear;
+    private int nItems;
 
-    public static void enqueue(Reservation r) {
-        queue.add(r);
+    public QueueArray(int s) {
+        maxSize = s;
+        queArray = new Reservation[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
     }
 
-    public static Reservation dequeue() {
-        return queue.poll();
+    public void insert(Reservation r) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (rear == maxSize - 1) rear = -1;
+            queArray[++rear] = r;
+            nItems++;
+        }
     }
 
-    public static Queue<Reservation> getQueue() {
-        return queue;
+    public Reservation remove() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return null;
+        } else {
+            Reservation temp = queArray[front++];
+            if (front == maxSize) front = 0;
+            nItems--;
+            return temp;
+        }
+    }
+
+    public boolean isEmpty() { return nItems == 0; }
+    public boolean isFull() { return nItems == maxSize; }
+
+    public Reservation[] toArray() {
+        Reservation[] result = new Reservation[nItems];
+        for (int i = 0, idx = front; i < nItems; i++) {
+            result[i] = queArray[idx];
+            idx = (idx + 1) % maxSize;
+        }
+        return result;
     }
 }
