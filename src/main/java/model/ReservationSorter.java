9f9c73b4c1065144254f0b19c002f6f7232172aa
sigma69-coder <sigmaboy77878@gmail.com>
2025-05-01 21:20:51 +0530

    Add ReservationSorter class

---
+++ src/main/java/model/ReservationSorter.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.Arrays;
+
+public class ReservationSorter {
+    public static Reservation[] mergeSortByTime(Reservation[] arr) {
+        if (arr.length <= 1) return arr;
+
+        int mid = arr.length / 2;
+        Reservation[] left = Arrays.copyOfRange(arr, 0, mid);
+        Reservation[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        return merge(mergeSortByTime(left), mergeSortByTime(right));
+    }
+
+    private static Reservation[] merge(Reservation[] left, Reservation[] right) {
+        Reservation[] result = new Reservation[left.length + right.length];
+        int i = 0, l = 0, r = 0;
+
+        while (l < left.length && r < right.length) {
+            if (left[l].getTime().compareTo(right[r].getTime()) <= 0) {
+                result[i++] = left[l++];
+            } else {
+                result[i++] = right[r++];
+            }
+        }
+
+        while (l < left.length) result[i++] = left[l++];
+        while (r < right.length) result[i++] = right[r++];
+        return result;
+    }
+}
