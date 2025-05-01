27ef8cc33398e68051fb8261abf374fd9ad8a9cc
sigma69-coder <sigmaboy77878@gmail.com>
2025-05-01 21:19:38 +0530

    Add seats in Reservation class

--- src/main/java/model/Reservation.java
+++ src/main/java/model/Reservation.java
@@ -5,16 +5,19 @@ public class Reservation {
     private String phone;
     private String date;
     private String time;
+    private int seats;
 
-    public Reservation(String name, String phone, String date, String time) {
+    public Reservation(String name, String phone, String date, String time, int seats) {
         this.name = name;
         this.phone = phone;
         this.date = date;
         this.time = time;
+        this.seats = seats;
     }
 
     public String getName() { return name; }
     public String getPhone() { return phone; }
     public String getDate() { return date; }
     public String getTime() { return time; }
+    public int getSeats() { return seats; }
 }
