2e50e6418b5672fc1c5304fdcbb4b98d38b57407
sigma69-coder <sigmaboy77878@gmail.com>
2025-05-08 15:34:26 +0530

    ReservationServlet.java updated

--- src/main/java/servlet/ReservationServlet.java
+++ src/main/java/servlet/ReservationServlet.java
@@ -10,15 +10,22 @@ import java.io.IOException;
 
 @WebServlet("/add-reservation")
 public class ReservationServlet extends HttpServlet {
+    private static QueueArray queue = new QueueArray(100);
+
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         String phone = req.getParameter("phone");
         String date = req.getParameter("date");
         String time = req.getParameter("time");
+        int seats = Integer.parseInt(req.getParameter("seats"));
 
-        Reservation res = new Reservation(name, phone, date, time);
-        QueueArray.enqueue(res);
+        Reservation res = new Reservation(name, phone, date, time, seats);
+        queue.insert(res);
 
-        resp.sendRedirect("viewQueue.jsp");
+        resp.sendRedirect("view-queue");
+    }
+
+    public static QueueArray getQueue() {
+        return queue;
     }
 }
