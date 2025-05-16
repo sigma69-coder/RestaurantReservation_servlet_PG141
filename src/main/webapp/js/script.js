266a2194699e9dbe2cafdf52c2050aa3d415843e
sigma69-coder <sigmaboy77878@gmail.com>
2025-05-16 20:33:06 +0530

    removed the Loging form

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,21 +1,3 @@
 console.log("Page Loaded");
-// Open login modal
-function openModal() {
-    document.getElementById("loginModal").style.display = "block";
-}
 
-// Close modal when clicking outside
-window.onclick = function(event) {
-    const modal = document.getElementById("loginModal");
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
 
-// Show bottom ad after page load
-window.onload = function() {
-    setTimeout(() => {
-        const ad = document.getElementById("adPopup");
-        if (ad) ad.style.display = "block";
-    }, 2500); // 2.5 seconds
-};
--- src/main/webapp/navbar.jsp
+++ src/main/webapp/navbar.jsp
@@ -2,5 +2,4 @@
     <a href="index.jsp">Home</a>
     <a href="reservationForm.jsp">Make Reservation</a>
     <a href="view-queue">View Queue</a>
-    <a href="#" class="login-btn" onclick="openModal()">Login</a>
 </div>
