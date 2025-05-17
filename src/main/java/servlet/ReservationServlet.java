e023b5a8c23fb3ecd036c9f55e560612cbe36684
Poornadeshan27 <poorna230deshan@gmail.com>
2025-05-17 23:50:25 +0530

    Add ReservationServlet to handle form submissions

--- src/main/java/servlet/ReservationServlet.java
+++ src/main/java/servlet/ReservationServlet.java
@@ -1,6 +1,6 @@
 package servlet;
 
- // Importing the required classes from the model packages
+// Importing the required classes from the model packages
 import model.Reservation;
 import model.QueueArray;
 
@@ -10,7 +10,7 @@ import javax.servlet.*;
 import java.io.IOException;
 
 @WebServlet("/add-reservation")
-public class ReservationServlet extends HttpServlet {
+    public class ReservationServlet extends HttpServlet {
     private static QueueArray queue = new QueueArray(100);
 
 
